85fa67c07fd9cf8fab8e6f4589e3259728bd5c8a
reviewpo95 <reviewpo95@gmail.com>
2019-08-05 20:38:09 +0900

    Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const Food = ({ name, img }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={img} />
+    </div>
+  );
+};
+
+const foodObject = [
+  {
+    name: "김치",
+    img:
+      "http://recipe1.ezmember.co.kr/cache/recipe/2015/04/02/238f0a3e8b567db1229b0f27220033341.jpg"
+  },
+  {
+    name: "감자",
+    img:
+      "https://file.mk.co.kr/meet/neds/2018/12/image_readmed_2018_775881_15446074643566869.png"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Movie-app</h1>
+      {foodObject.map(ha => (
+        <Food name={ha.name} img={ha.img} />
+      ))}
     </div>
   );
 }
