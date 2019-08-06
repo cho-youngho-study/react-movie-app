d27c31120dae105f4cee085786da779c704224ac
reviewpo95 <reviewpo95@gmail.com>
2019-08-06 11:29:10 +0900

    state study

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Food = ({ name, img }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <img src={img} />
-    </div>
-  );
-};
-
-const foodObject = [
-  {
-    name: "김치",
-    img:
-      "http://recipe1.ezmember.co.kr/cache/recipe/2015/04/02/238f0a3e8b567db1229b0f27220033341.jpg"
-  },
-  {
-    name: "감자",
-    img:
-      "https://file.mk.co.kr/meet/neds/2018/12/image_readmed_2018_775881_15446074643566869.png"
+class App extends Component {
+  state = {
+    count: 0
+  };
+  handleOnClick_add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  handleOnClick_min = () => {
+    const { count } = this.state;
+    this.setState({
+      count: count - 1
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.state.count}</h1>
+        <button onClick={this.handleOnClick_add}>Add</button>
+        <button onClick={this.handleOnClick_min}>min</button>
+      </div>
+    );
   }
-];
-
-function App() {
-  return (
-    <div>
-      <h1>Movie-app</h1>
-      {foodObject.map(ha => (
-        <Food name={ha.name} img={ha.img} />
-      ))}
-    </div>
-  );
 }
 
 export default App;
