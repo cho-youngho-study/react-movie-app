aa076c7e61f9a4b663f2dc09c482d7308117b4f3
reviewpo95 <reviewpo95@gmail.com>
2019-08-06 14:55:58 +0900

    Updates

---
+++ precache-manifest.69fb9f48cf4a9c54a544ecaaefb36218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79565603949852a6ef50c2c80c6b0525",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "f305e2f60658ecc68598",
+    "url": "/react-movie-app/static/css/main.8063d542.chunk.css"
+  },
+  {
+    "revision": "0ec6eb4f52f4c35640e7",
+    "url": "/react-movie-app/static/js/2.87cac49b.chunk.js"
+  },
+  {
+    "revision": "f305e2f60658ecc68598",
+    "url": "/react-movie-app/static/js/main.63d27097.chunk.js"
+  },
+  {
+    "revision": "e0c3c7f83a8b3d0de128",
+    "url": "/react-movie-app/static/js/runtime~main.4dfe89d3.js"
+  }
+]);
